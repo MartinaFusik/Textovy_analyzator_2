c421cc2ce2a498b677b8f79f30a6f787f12717c5
MartinaFusik <martina.fusikova@gmail.com>
2022-05-29 22:35:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# my-github-repo
+I will test my Git knowledge on this repo.
+I have added new text into this file.       # Přidáváme tento řádek.
+*.pyc                                       # Ignoruj všechny soubory s příponou .pyc
+.idea/                                      # Ignoruj všechny soubory v adresáři .idea/

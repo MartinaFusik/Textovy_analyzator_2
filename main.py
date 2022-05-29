4da747932f2ead791d877129c5be983742887b15
MartinaFusik <martina.fusikova@gmail.com>
2022-05-29 23:24:53 +0200

    Zacatek

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-# my-github-repo
-I will test my Git knowledge on this repo.
-I have added new text into this file.       # Přidáváme tento řádek.
-*.pyc                                       # Ignoruj všechny soubory s příponou .pyc
-.idea/                                      # Ignoruj všechny soubory v adresáři .idea/
+git config user.name "Martina Fúsiková"
+git config user.email "martina.fusikova@gmail.com"
